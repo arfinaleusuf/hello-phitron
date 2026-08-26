41d6af1d3e2c244ed9d192b9a670fc65598de15b
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-27 02:49:09 +0600

    Add window resize listener to Counter component and display monitor width

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Count from "./Count";
 
 const Counter = () => {
     const [counter, setCounter] = useState(0)
+    const[width, setWidth] = useState(0)
 
     // console.log(counter, "counter")
+
+    useEffect(()=> {
+        document.title = "counter"
+        localStorage.setItem("useEffect", "Use effect treggerd")
+        
+        window.addEventListener("resize", ()=> {
+            setWidth(window.innerWidth);
+        })
+    })
 
     const counterHandler =  () =>{
         setCounter((prevCounter) => prevCounter + 1)
@@ -15,6 +25,8 @@ const Counter = () => {
         <div className="counter">
             <Count counter={counter} setCounter={setCounter}/>
             <button onClick={()=> counterHandler()} className="increment-btn">Increment</button>
+
+            <h2>monitor width: {width}</h2>
         </div>
     );
 };
