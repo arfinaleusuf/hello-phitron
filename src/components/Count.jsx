5c9964c5f04c02f118383aefd2faeb64b3824c92
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-27 01:04:07 +0600

    Add SVG assets and implement counter component
    - Added react.svg and vite.svg assets to the project.
    - Created Count component to display and update counter value.
    - Implemented Counter component to manage counter state and handle increment logic.
    - Added basic styles for counter and increment button in index.css.
    - Set up main entry point in main.jsx to render the Counter component.
    - Configured Vite with React plugin in vite.config.js.

---
+++ src/components/Count.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Count = ({ counter, setCounter}) => {
+
+    return <h2>
+        <button onClick={()=> setCounter(100)}>Update</button>
+        {counter}</h2>;
+};
+
+export default Count;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Count from "./Count";
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0)
+
+    console.log(counter, "counter")
+
+    const counterHandler =  () =>{
+        setCounter((prevCounter) => prevCounter + 1)
+        setCounter((prevCounter) => prevCounter + 1)
+        setCounter((prevCounter) => prevCounter + 1)
+    }
+    return (
+        <div className="counter">
+            <Count counter={counter} setCounter={setCounter}/>
+            <button onClick={()=> counterHandler()} className="increment-btn">Increment</button>
+        </div>
+    );
+};
+
+export default Counter;
